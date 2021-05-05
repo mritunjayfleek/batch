8a9434cb5bbf8dc7848b18f7161e29e0acee9080
mritunjay-bst <mritunjay@batchdialer.com>
2021-05-05 18:37:15 +0530

    Initial_commit

---
+++ cypress/integration/register.spec.js
@@ -0,0 +1,166 @@
+import Register from "../support/pages/Register";
+let fixtureData;
+let randNum = Math.floor(Math.random() * 1000);
+
+describe("Failed Register with Empty Fields", () => {
+  before(() => {
+    // To add random number in constants.json
+    cy.readFile("cypress/fixtures/constants.json", (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+    }).then((data) => {
+      data.randNum = randNum;
+      cy.writeFile("cypress/fixtures/constants.json", JSON.stringify(data));
+    });
+
+    cy.fixture("constants")
+      .then((data) => (fixtureData = data))
+      .then(() => {
+        cy.visit("/", { failOnStatusCode: false });
+      });
+  });
+
+  it("SKT-T1: Verify all elements on Register page", () => {
+    const regpage = new Register();
+    regpage.clickSignUpNowButton();
+    regpage.verifyRegisterFields("firstName");
+    regpage.verifyRegisterFields("lastName");
+    regpage.verifyRegisterFields("email");
+    regpage.verifyRegisterFields("companyName");
+    regpage.verifyRegisterFields("mobileNumber");
+    regpage.verifyRegisterFields("password");
+    regpage.verifyRegisterFields("confirmPassword");
+    regpage.verifyBackToLoginLink();
+    regpage.verifyRegisterButton();
+  });
+
+  it("SKT-T2: Verify Required Fields", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.clickRegisterButton();
+    regpage.verifyRequiredField("firstName");
+    regpage.verifyRequiredField("lastName");
+    regpage.verifyRequiredField("email");
+    regpage.verifyMobileInputFieldError("mobileNumber");
+    regpage.verifyRequiredField("password");
+    regpage.verifyRequiredField("confirmPassword");
+  });
+
+  it("SKT-T3: Verify validation on First name and Last name", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName("1234");
+    regpage.enterLastName("5678");
+    regpage.enterEmail(fixtureData.email);
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(fixtureData.MobileNumber);
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyValidationOnFirstLastField("firstName");
+    regpage.verifyValidationOnFirstLastField("lastName");
+  });
+
+  it("SKT-T4: Verify validation on Phone Number", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(fixtureData.firstName);
+    regpage.enterLastName(fixtureData.lastName);
+    regpage.enterEmail(fixtureData.email);
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber("9999");
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyMobileValidation("mobileNumber");
+  });
+
+  it("SKT-T5: Verify validation on email address", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(fixtureData.firstName);
+    regpage.enterLastName(fixtureData.lastName);
+    regpage.enterEmail("invalid");
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(fixtureData.MobileNumber);
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyEmailValidation("email");
+  });
+
+  it("SKT-T103: Verify user should not be registered with empty first name.", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(" ");
+    regpage.enterLastName(fixtureData.lastName);
+    regpage.enterEmail(fixtureData.email);
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(fixtureData.MobileNumber);
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyMandatoryFieldError("firstName");
+  });
+
+  it("SKT-T104: Verify user should not be registered with empty last name.", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(fixtureData.firstName);
+    regpage.enterLastName(" ");
+    regpage.enterEmail(fixtureData.email);
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(fixtureData.MobileNumber);
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyMandatoryFieldError("lastName");
+  });
+
+  it("SKT-T105: Verify user should not be registered with empty email field.", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(fixtureData.firstName);
+    regpage.enterLastName(fixtureData.lastName);
+    regpage.enterEmail(" ");
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(fixtureData.MobileNumber);
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyMandatoryFieldError("email");
+  });
+
+  it("SKT-T106: Verify user should not be registered with empty phone number.", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(fixtureData.firstName);
+    regpage.enterLastName(fixtureData.lastName);
+    regpage.enterEmail(fixtureData.email);
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(" ");
+    regpage.enterPassword(fixtureData.password);
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyMobileInputFieldError("mobileNumber");
+  });
+
+  it("SKT-T107: Verify user should not be registered with empty password and confirm password", () => {
+    const regpage = new Register();
+    cy.reload();
+    regpage.enterFirstName(fixtureData.firstName);
+    regpage.enterLastName(fixtureData.lastName);
+    regpage.enterEmail(fixtureData.email);
+    regpage.enterCompanyName(fixtureData.companyName);
+    regpage.enterMobileNumber(fixtureData.MobileNumber);
+    regpage.enterPassword(" ");
+    regpage.enterConfirmPassword(fixtureData.confirmPassword);
+    regpage.clickRegisterButton();
+    regpage.verifyMandatoryFieldError("password");
+    regpage.enterPassword(fixtureData.password);
+    regpage.clearForgetPassword();
+    regpage.clickRegisterButton();
+    regpage.verifyMandatoryFieldError("confirmPassword");
+  });
+});
---
+++ cypress/support/pages/Register.js
@@ -0,0 +1,134 @@
+import { expect } from "chai";
+
+const signUpNowBtn = '//button[span[contains(text(),"SIGN UP NOW")]]';
+const firstNameInputField = 'input[formcontrolname="firstName"]';
+const lastNameInputField = 'input[formcontrolname="lastName"]';
+const emailInputField = 'input[formcontrolname="email"]';
+const compNameInput = 'input[formcontrolname="companyName"]';
+const mobileNumberInput = 'input[formcontrolname="mobileNumber"]';
+const passwordInput = 'input[formcontrolname="password"]';
+const confirmPasswordIinput = 'input[formcontrolname="confirmPassword"]';
+const registerButton = 'button[type="submit"]';
+const backToLoginLink = "a.bottom-log-link";
+const registerField = (regField) => `input[formcontrolname="${regField}"]`;
+const mandatoryFieldError = (inputField) =>
+  `input[formcontrolname="${inputField}"] +bfv-messages span.invalid-feedback`;
+const firstLastNameValidation = (firstlast) =>
+  `input[formcontrolname="${firstlast}"] +bfv-messages span.invalid-feedback`;
+const loginButton = 'button[type="submit"]';
+const duplicateEmailError =
+  '//div[@role="alertdialog" and contains(text(),"is already in use")]';
+const profilePic = ".profile-pic";
+const logout = ".log-out";
+
+export default class Register {
+  clickSignUpNowButton() {
+    cy.xpath(signUpNowBtn).click();
+  }
+
+  enterFirstName(first_name) {
+    cy.get(firstNameInputField).type(first_name);
+  }
+
+  enterLastName(last_name) {
+    cy.get(lastNameInputField).type(last_name);
+  }
+
+  enterEmail(email) {
+    cy.get(emailInputField).type(email);
+  }
+
+  enterCompanyName(comp_name) {
+    cy.get(compNameInput).type(comp_name);
+  }
+
+  enterMobileNumber(mobileNum) {
+    cy.get(mobileNumberInput).type(mobileNum);
+  }
+
+  enterPassword(password) {
+    cy.get(passwordInput).type(password);
+  }
+
+  clearForgetPassword() {
+    cy.get(confirmPasswordIinput).clear();
+  }
+
+  enterConfirmPassword(confirmPassword) {
+    cy.get(confirmPasswordIinput).type(confirmPassword);
+  }
+
+  clickRegisterButton() {
+    cy.get(registerButton).click();
+  }
+
+  verifyRegisterButton() {
+    cy.get(registerButton).should("be.visible");
+  }
+
+  clickBackToLoginLink() {
+    cy.get(backToLoginLink).click();
+  }
+
+  verifyBackToLoginLink() {
+    cy.get(backToLoginLink).should("be.visible");
+  }
+
+  verifyRegisterFields(allField) {
+    cy.get(registerField(allField)).should("be.visible");
+  }
+
+  verifyRequiredField(mand) {
+    cy.get(mandatoryFieldError(mand)).should("be.visible");
+  }
+
+  verifyValidationOnFirstLastField(inputField) {
+    cy.get(firstLastNameValidation(inputField)).then((errText) => {
+      expect(errText.text()).to.contain("can contain only alphabets");
+    });
+  }
+
+  verifyMobileValidation(mobile) {
+    cy.get(mandatoryFieldError(mobile)).then((errText) => {
+      expect(errText.text()).to.contain("must be at least 10 characters");
+    });
+  }
+
+  verifyEmailValidation(email) {
+    cy.get(mandatoryFieldError(email)).then((errText) => {
+      expect(errText.text()).to.contain("Invalid email address");
+    });
+  }
+
+  verifyMobileInputFieldError(mobile) {
+    cy.get(mandatoryFieldError(mobile)).then((errName) => {
+      expect(errName.text()).to.contain("This field is required");
+    });
+  }
+
+  verifyMandatoryFieldError(mobile) {
+    cy.get(mandatoryFieldError(mobile)).then((errName) => {
+      expect(errName.text()).to.contain(`can't be blank`);
+    });
+  }
+
+  verifyLoginButton() {
+    cy.get(loginButton).should("be.visible");
+  }
+
+  verifyDuplicateEmailError() {
+    cy.xpath(duplicateEmailError).should("be.visible");
+  }
+
+  verifySuccessfullRegister() {
+    cy.get(profilePic, { timeout: 15000 }).should("be.visible");
+  }
+
+  clickLogout() {
+    cy.wait(1000);
+    cy.get(profilePic).click();
+    cy.wait(1000);
+    cy.get(logout).click();
+    cy.get(loginButton, { timeout: 10000 }).should("be.visible");
+  }
+}
